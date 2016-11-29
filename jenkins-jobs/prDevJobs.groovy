d80326d3745891a2406b13e3bfaf00925bcb9c2a
tabbi89 <tom@tabbi89.com>
2016-11-29 20:06:32 +0100

    Fix

--- jenkins-jobs/prDevJobs.groovy
+++ jenkins-jobs/prDevJobs.groovy
@@ -6,22 +6,39 @@ folder(basePath) {
 }
 
 job("$basePath/PRDev") {
-    label "digital-slave"
-    scm {
-        git {
-            remote {
-                github repo
-                refspec '+refs/pull/*:refs/remotes/origin/pr/*'
+        label "digital-slave"
+        scm {
+            git {
+                remote {
+                    github repo
+                    refspec '+refs/pull/*:refs/remotes/origin/pr/*'
+                }
+                branch('${sha1}')
             }
-            branch('${sha1}')
         }
-    }
-    triggers {
-        githubPullRequest {
-            admin 'tabbi89'
-            triggerPhrase('OK to test')
-            useGitHubHooks()
-            cron('* * * * *')
+        triggers {
+                githubPullRequest {
+                    admin 'tabbi89'
+                    triggerPhrase('OK to test')
+                    useGitHubHooks()
+                    cron('* * * * *')
+                    allowMembersOfWhitelistedOrgsAsAdmin()
+                    extensions {
+                        buildStatus {
+                            completedStatus('SUCCESS', 'There were no errors, go have a cup of coffee...')
+                            completedStatus('FAILURE', 'There were errors, for info, please see...')
+                            completedStatus('ERROR', 'There was an error in the infrastructure, please contact...')
+                        }
+                    }
+                }
+        }
+        publishers {
+            mergeGithubPullRequest {
+                mergeComment('merged by Jenkins')
+                onlyAdminsMerge()
+                failOnNonMerge()
+                deleteOnMerge()
+            }
         }
     }
     steps {
