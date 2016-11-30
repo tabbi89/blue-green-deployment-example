f16ff37ff3783c90fef06835bdfd431c9cf64a20
tabbi89 <tom@tabbi89.com>
2016-11-30 12:28:43 +0100

    WIP

--- jenkins-jobs/deployToProductionJobs.groovy
+++ jenkins-jobs/deployToProductionJobs.groovy
@@ -13,7 +13,7 @@ pipelineJob("$basePath/deploy-to-production") {
             sandbox()
             script("""
                 node('digital-slave') {
-                    git url: 'https://github.com/tabbi89/blue-green-deployment-example', branch: 'master'
+                    git url: 'https://github.com/tabbi89/blue-green-deployment-example', branch: 'master', credentialsId: 'b2cad6ea-8de0-4d36-882a-87d586cfc9d0'
 
                     stage("Install dependencies") {
 
--- jenkins-jobs/rollbackProductionJobs.groovy
+++ jenkins-jobs/rollbackProductionJobs.groovy
@@ -10,7 +10,7 @@ pipelineJob("$basePath/rollback-production") {
             sandbox()
             script("""
                 node('digital-slave') {
-                    git url: 'https://github.com/tabbi89/blue-green-deployment-example'
+                    git url: 'https://github.com/tabbi89/blue-green-deployment-example', credentialsId: 'b2cad6ea-8de0-4d36-882a-87d586cfc9d0'
 
                     stage("Switch back non active node to live") {
                         ansiblePlaybook  playbook: 'ansible/switch_env.yml', inventory: 'ansible/inventory/production'
